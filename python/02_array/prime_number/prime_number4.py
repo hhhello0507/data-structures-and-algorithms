abd00d758683e6bcc008cec15c078ac18bed3331
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-27 15:29:14 +0900

    Study :: more more improve prime number func

---
+++ python/02_array/prime_number/prime_number4.py
@@ -0,0 +1,18 @@
+n = 31
+
+def get_primes_until(n):
+    prime_number_list = [2]
+    for i in range(3, n + 1):
+        t = True
+        for prime_number in prime_number_list:
+            if prime_number ** 0.5 > i:
+                break
+            if i % prime_number == 0:
+                t = False
+                break
+        if t:
+            prime_number_list.append(i)
+    return prime_number_list
+
+result = get_primes_until(n)
+print(result)
