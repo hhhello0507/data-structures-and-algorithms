bac6a2826c6ded038143443992a88ca584b564e9
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-28 19:48:33 +0900

    Study :: bubble sort

---
+++ python/06_sort/bubble_sort/bubble_sort.py
@@ -0,0 +1,12 @@
+import random
+
+n = 10
+
+arr = [random.randint(0, n) for _ in range(n)]
+
+for i in range(n):
+    for j in range(n - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print(arr)
