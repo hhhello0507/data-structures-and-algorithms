0731328aa6877cceece7d7b178ca2c3bd5c124da
hhhello <hhhello0507@dgsw.hs.kr>
2024-07-06 21:26:02 +0900

    Improve :: get_primes_until

--- python/02_array/prime_number/prime_number4.py
+++ python/02_array/prime_number/prime_number4.py
@@ -2,7 +2,7 @@ n = 31
 
 def get_primes_until(n):
     prime_number_list = [2]
-    for i in range(3, n + 1):
+    for i in range(3, n + 1, 2):
         t = True
         for prime_number in prime_number_list:
             if prime_number ** 0.5 > i:
